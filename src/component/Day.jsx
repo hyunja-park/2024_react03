b50f2510f8d476e67f6fcb4a2eec34e4f3c35573
parkhyunja <hjpark20121125@gmail.com>
2024-11-25 12:19:01 +0900

    db전내용

---
+++ src/component/Day.jsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import dummy from "../db/data.json"
+import Word from "./Word";
+
+function Day(props) {
+    // const day = 1;
+
+    // url에 포함된 day값을 가져오기 위해서 useParams() 1.2.3을 말함
+    const day = useParams().day;
+    const wordList = dummy.words.filter(k => (k.day) === Number(day))
+    return (
+        <table>
+           <tbody>
+                {/* {dummy.words.map(k => { */}
+                {wordList.map(k =>{
+                    return <Word key={k.id} word={k} />
+                })}
+            </tbody> 
+        </table>
+    );
+}
+
+export default Day;
